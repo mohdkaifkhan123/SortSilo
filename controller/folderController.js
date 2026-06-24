8ad75d55a8fcecca39bff589fb777b4c4bdfce99
mohdkaifkhan123 <khank11111999@gmail.com>
2026-06-24 23:28:54 +0530

    Folder feature bug fix

--- controller/folderController.js
+++ controller/folderController.js
@@ -1,20 +1,26 @@
 import prisma from "../prisma/prismaClient.js";
 
-export const createFolder = async (req,res) => {
+export const createFolder = async (req, res) => {
   const { folderName, parentId } = req.body;
-    console.log("jhj",req.userId,folderName, parentId )
+  console.log("jhj", req.userId, folderName, parentId);
 
-   if (parentId) {
-      const parent = await prisma.folder.findUnique({ where: { id: parseInt(parentId) } });
-      if (!parent || parent.UserId !== req.userId) {
-        return res.status(403).json({ message: "Parent folder not found or unauthorized" });
-      }
+  if (parentId) {
+    const parent = await prisma.folder.findUnique({
+      where: { id: parseInt(parentId) },
+    });
+    if (!parent || parent.UserId !== req.userId) {
+      return res
+        .status(403)
+        .json({ message: "Parent folder not found or unauthorized" });
     }
+  }
   try {
     const finalRes = await prisma.folder.create({
-      folderName: folderName,
-      UserId: req.userId,
-      parentId: parentId,
+      data: {
+        folderName: folderName,
+        UserId: req.userId,
+        parentId: parentId,
+      },
     });
     res.status(200).json({ finalRes });
   } catch (error) {
